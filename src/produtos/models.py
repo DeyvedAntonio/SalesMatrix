4aa8e06ad664d612bbe3e7dd15d5d7abce89d1fa
Deyved Antonio <deyved.antonio@gmail.com>
2026-02-24 20:08:31 -0300

    feat: criação do model de produtos.

--- src/produtos/models.py
+++ src/produtos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from categorias.models import Category
+
+
+class Product(models.Model):
+
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Nome do Produto',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='fk_produto_categoria',
+        verbose_name='Categoria do Produto',
+    )
+    unit_price = models.DecimalField(
+        verbose_name='Preço Unitário do Produto',
+    )
+
+    class Meta:
+
+        ordering = ['name', 'category']
+
+    def __str__(self) -> str:
+
+        return f'{self.name} - {self.unit_price}'

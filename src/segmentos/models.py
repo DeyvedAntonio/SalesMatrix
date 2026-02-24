a2ec4a66a1cbd1f4cc11647e65e1080de9689fbd
Deyved Antonio <deyved.antonio@gmail.com>
2026-02-24 20:58:46 -0300

    feat: criação do model de segmentos.

--- src/segmentos/models.py
+++ src/segmentos/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Segments(models.Model):
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Nome do Segmento'
+    )
+    description = models.TextField(
+        verbose_name='Descrição do Segmento',
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name='Data de Criação'
+    )
+    modified = models.DateField(
+        auto_now=True,
+        verbose_name='Data de Modificação'
+    )
+
+    class Meta:
+
+        ordering = ['name',]
+
+    def __str__(self) -> str:
+        return self.name
